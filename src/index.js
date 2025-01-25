97fe682bd4c999d5bb1aa98cf71b3fbf13f23068
Hugo Quezada <hquezada@leomail.tamuc.edu>
2025-01-25 12:52:47 -0600

    create event listener to show and hide the menu. Change h1 and drop-down-menu div container flex direction to column.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+// elements
+const button = document.getElementById("drop-down-menu-button");
+
+button.addEventListener("click", () => {
+  const menu = document.getElementById("nav-links-container");
+
+  if (menu.classList.contains("hidden")) {
+    // show the menu by removing the hidden class
+    menu.classList.remove("hidden");
+  } else if (!menu.classList.contains("hidden")) {
+    // hide the menu by adding the hidden class
+    menu.classList.add("hidden");
+  }
+});
